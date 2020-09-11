514e54003005dfc7b9ffa3a195f3a96c4ebdef58
HieuDV <huuhieu110597@gmail.com>
2020-09-12 00:02:40 +0700

    project nhe

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>anh hiếu đẹp zai</Text>
+    </View>
+  );
+};
+
+export default App;
